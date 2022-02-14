d458914e6b304c6c0cfda8a416b5a8820f1caf40
Amy Young <youngamy1223@gmail.com>
2022-02-13 17:18:55 -0700

    add Simple Madlib option

--- MadLibs/Controllers/HomeController.cs
+++ MadLibs/Controllers/HomeController.cs
@@ -8,7 +8,7 @@ namespace MadLibs.Controllers
     [Route("/form")]
     public ActionResult Form() { return View(); }
   
-    [Route("/madlib")]
+    [Route("/madlib1")]
     public ActionResult ComplexMadLib(string adjective1, string noun1, string verbPT1, string adverb1, string adjective2, string noun2, string noun3, string adjective3, string verb2, string adverb2, string verbPT2, string adjective4)
     {
       LibsVariable newLibsVariable = new LibsVariable();
@@ -24,6 +24,17 @@ namespace MadLibs.Controllers
       newLibsVariable.Adverb2 = adverb2;
       newLibsVariable.VerbPT2 = verbPT2;
       newLibsVariable.Adjective4 = adjective4;
+      return View(newLibsVariable);
+    }
+
+    [Route("/madlib2")]
+    public ActionResult SimpleMadLib(string adjective1, string noun1, string verbPT1)
+    {
+      LibsVariable newLibsVariable = new LibsVariable();
+      newLibsVariable.Adjective1 = adjective1;
+      newLibsVariable.Noun1 = noun1;
+      newLibsVariable.VerbPT1 = verbPT1;
+    
       return View(newLibsVariable);
     }
   }
--- MadLibs/Models/LibsVariable.cs
+++ MadLibs/Models/LibsVariable.cs
@@ -4,17 +4,16 @@ namespace MadLibs.Models
   {
     public string Adjective1 { get; set; }
     public string Noun1 { get; set; }
-    public string VerbPT1 {get; set; }
-    public string Adverb1 {get; set; }
+    public string VerbPT1 { get; set; }
+    public string Adverb1 { get; set; }
     public string Adjective2 { get; set; }
     public string Noun2 { get; set; }
-    public string Noun3 {get; set; }
-    public string Adjective3 {get; set; }
+    public string Noun3 { get; set; }
+    public string Adjective3 { get; set; }
     public string Verb2 { get; set; }
-    public string Adverb2 {get; set; }
-    public string VerbPT2 {get; set; }
-    public string Adjective4 {get; set; }
-    
+    public string Adverb2 { get; set; }
+    public string VerbPT2 { get; set; }
+    public string Adjective4 { get; set; }
   }
 }
 
